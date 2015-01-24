6ab09134c051d7c439cab20029ec2b6b568d0417
liskbpet <liskbpet@WL042988.eu.tieto.com>
2015-01-24 09:34:09 +0100

    latest update

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -1,17 +1,17 @@
 import os
 
-dir_fd = os.open('C:\tmp\', os.O_RDONLY)
+#dir_fd = os.open('C:\tmp\', os.O_RDONLY)
 
-def opener(path, flags):
-    return os.open(path, flags, dir_fd=dir_fd)
+#def opener(path, flags):
+#    return os.open(path, flags, dir_fd=dir_fd)
 
-with open('test.txt', 'r', opener=opener) as f:
-     print('This will be written to somedir/spamspam.txt', file=f)
+#with open('test.txt', 'r', opener=opener) as f:
+#     print('This will be written to somedir/spamspam.txt', file=f)
 
-os.close(dir_fd)  # don't leak a file descriptor
+#os.close(dir_fd)  # don't leak a file descriptor
 #fo = open("test.txt", "wb")
 #str = fo.read(1)
 #print(str)
 #fo.close()
-print("exiting...")
-
+#print("exiting...")
+print("Miska je nejlepsi")
